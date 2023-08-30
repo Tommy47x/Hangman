938db85f393d67b20996bd32fc32efb67fedb3bf
Tommy47x <110035943+Tommy47x@users.noreply.github.com>
2023-08-30 14:52:24 +0300

    Update script.js 2
    -divided the 'guessLetter' function into 3 functions (updateGame, validateInput and guessLetter).
    -the code works exactly the same, but more efficient now and we got rid of 4 global variables.

--- script.js
+++ script.js
@@ -1,74 +1,72 @@
-let copyLetters; // Global variables defined here
-let remainingAttempts = 7;
-let randomWord = "";
-let guessedWord = "";
-let resultContainer = "";
-let wordContainer = null;
-let attemptsContainer = null;
-let restrictionInput = "";
+let usedLetters; // In-page shown letters
+let remainingAttempts = 7; // Number of attempts, 0 = fail
+let randomWord = ""; // var. for algorithm of choosing random word
+let guessedWord = ""; // replacement with '_'
 
-function startGame() {
-  copyLetters = document.getElementById("letters-used");
-  copyLetters.textContent = "Used letters:"; // Reset letters
-  remainingAttempts = 7; //Reset attempts
-  document.getElementById
-    ("guess-button").disabled = false; // Reset 'Start'
-  const words = ["wellcode", "programming", "tommy", "wisdom",
-    "javascript", "frontend"];
-  randomWord = words[Math.floor(Math.random() * words.length)];
-  guessedWord = "_".repeat(randomWord.length);
-  resultContainer = document.getElementById("result");
-  resultContainer.innerHTML = ""; // Reset in-page message
-  wordContainer = document.getElementById("word-container");
-  wordContainer.textContent = guessedWord.split("").join(" ");
-  attemptsContainer = document.getElementById("attempts");
-  attemptsContainer.textContent = ""; // Reset in-page attempts
-
+function startGame() { //Function called on 'Start' button
+  usedLetters = document.getElementById("letters-used");// In-page shown letters
+  usedLetters.textContent = "Used letters:"; // Reset 
+  remainingAttempts = 7; // Reset
+  document.getElementById("guess-button").disabled = false; // Reset Button
+  const words = ["wellcode", "programming", "tommy", "wisdom", "javascript", "frontend"];
+  randomWord = words[Math.floor(Math.random() * words.length)]; //Algorithm choosing the word
+  guessedWord = "_".repeat(randomWord.length); // Replacement with '_'
+  document.getElementById("result").innerHTML = ""; // Reset
+  document.getElementById("word-container").textContent = guessedWord.split("").join(" "); // Hidden word in-page
+  document.getElementById("attempts").textContent = ""; // Reset
 }
 
-function guessLetter() {
-  restrictionInput = document.getElementById("alerting");
-  //Input + Restrictions
-  let guessedLetterInput = document.getElementById("input-text");
-  let guessedLetter = guessedLetterInput.value.toLowerCase();
+function validateInput(guessedLetter) { // Restrictions for input
+  const restrictionInput = document.getElementById("alerting");
   if (guessedLetter.length !== 1 || !guessedLetter.match(/[a-z]/i)) {
     restrictionInput.textContent = "Please enter a single valid letter";
-    guessedLetterInput.value = "";
+    return false;
   }
-  let lettersUsed = document.getElementById("letters-used");
+  restrictionInput.textContent = " ";
+  return true;
+}
+
+function updateGame(guessedLetter) {// Function that verifies letters
   let found = false; // For decreasing attempts
   let updatedGuessedWord = "";
-  for (let i = 0; i < randomWord.length; ++i) { // Verifying letters
+  for (let i = 0; i < randomWord.length; ++i) {
     if (randomWord[i] === guessedLetter) {
-      updatedGuessedWord += guessedLetter;
+      updatedGuessedWord += guessedLetter; // Adds correct letters for '____' word
       found = true;
-      restrictionInput.textContent = " ";
     } else {
       updatedGuessedWord += guessedWord[i];
     }
   }
 
-  if (updatedGuessedWord === randomWord) { // Message Succes
-    document.getElementById
-      ("guess-button").disabled = true;
-    resultContainer.innerHTML =
+  if (updatedGuessedWord === randomWord) { // Winner message
+    document.getElementById("guess-button").disabled = true; // Disabling button
+    document.getElementById("result").innerHTML =
       `<p class="text-success">You won! The word was "${randomWord}".</p> `;
   }
 
-  if (!found) {
-    --remainingAttempts; //Decrease attempts
+  if (!found) { // Decrease attempts
+    --remainingAttempts;
   }
 
-  if (remainingAttempts === 0) { // Message Fail
-    document.getElementById("guess-button").disabled = true;
-    resultContainer.innerHTML = `
+  if (remainingAttempts === 0) { // Loser message
+    document.getElementById("guess-button").disabled = true; // Disabling button
+    document.getElementById("result").innerHTML = `
       <p class="text-danger">Game over! The word was "${randomWord}". Press 'Start' to try again!</p>`;
   }
-  lettersUsed.textContent += guessedLetter + " "; // In-page letters
-  guessedWord = updatedGuessedWord; // In-page letter replacement
-  wordContainer.textContent = guessedWord.split("").join(" ");// In-page letter replacement
-  guessedLetterInput.value = ""; // Reset input
-  attemptsContainer.textContent = `Remaining attempts:
-  ${remainingAttempts}`;
 
+  guessedWord = updatedGuessedWord; // Var. for changing in-page letters, to match the word
+  document.getElementById("word-container").textContent = guessedWord.split("").join(" "); // In-page letters
+  document.getElementById("attempts").textContent = `Remaining attempts: ${remainingAttempts}`; // Attempts
+}
+
+function guessLetter() { // Final function that enables all functions (Exception: StartGame)
+  const guessedLetterInput = document.getElementById("input-text"); 
+  const guessedLetter = guessedLetterInput.value.toLowerCase();
+
+  if (validateInput(guessedLetter)) {
+    updateGame(guessedLetter);
+    usedLetters.textContent += guessedLetter + " ";
+    guessedLetterInput.value = ""; // Reset Input
+  }
+  guessedLetterInput.value = ""; // Reset Input for mistakes
 }
